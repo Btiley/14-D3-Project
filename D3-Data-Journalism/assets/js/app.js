30ea3fb295c7296bfd107a7b002007a29a1af474
Brandon Tiley <bctiley@gmail.com>
2021-03-27 13:00:38 +1100

    Added SVG element and created margins

--- D3-Data-Journalism/assets/js/app.js
+++ D3-Data-Journalism/assets/js/app.js
@@ -1,3 +1,27 @@
+//Create SVG
+//-------------------------------------------------------------------
+
+//Set Margins & SVG size
+var svgWidth = 750;
+var svgHeight = 350;
+
+var margin = {top: 10, right: 30, bottom: 30, left: 60}
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Append SVG to hold chart.
+var svg = d3.select("#scatter")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  
+//Shift svg by left and top margins.
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+//CREATE CHART
+//-------------------------------------------------------------------
+
 //Call data from csv
 d3.csv("assets/data/data.csv", function(data) { 
     
